15e8865c047972c76fc7ca5b0dc1ad90a2c26ad3
AA-Rexford <AA-Rexford@users.noreply.github.com>
2026-07-26 16:04:09 +0000

    Update to full 3-light vertical tower with dimmed OFF states

--- traffic.asm
+++ traffic.asm
@@ -1,64 +1,128 @@
 section .data
-    ; --- Huge ASCII Art Circular Light Messages ---
-    ; Using ANSI escape codes [1;31m, [38;5;226m, [1;32m for EXTREMELY BRIGHT colors
-    ; Mathematical perfectly symmetrical 23-character lines.
+    ; --- 3-Light Tower Frames ---
+    ; ANSI codes: [1;31m (Bright Red), [38;5;226m (Bright Yellow), [1;32m (Bright Green), [1;30m (Dark Gray)
     
-    red_msg db 0x1B, '[1;31m'
-            db '      .---------.      ', 0xA
-            db '    ."           ".    ', 0xA
-            db '   /      RED      \   ', 0xA
-            db '  |      STOP!      |  ', 0xA
-            db '   \               /   ', 0xA
-            db '    ".           ."    ', 0xA
-            db '      `---------`      ', 0xA
-            db 0x1B, '[0m', 0xA
-    red_len equ $ - red_msg
+    red_frame_msg db 0x1B, '[1;31m'
+                  db '      .---------.      ', 0xA
+                  db '    ."           ".    ', 0xA
+                  db '   /      RED      \   ', 0xA
+                  db '  |      STOP!      |  ', 0xA
+                  db '   \               /   ', 0xA
+                  db '    ".           ."    ', 0xA
+                  db '      `---------`      ', 0xA
+                  db 0x1B, '[1;30m'
+                  db '                       ', 0xA
+                  db '      .---------.      ', 0xA
+                  db '    ."           ".    ', 0xA
+                  db '   /               \   ', 0xA
+                  db '  |                 |  ', 0xA
+                  db '   \               /   ', 0xA
+                  db '    ".           ."    ', 0xA
+                  db '      `---------`      ', 0xA
+                  db '                       ', 0xA
+                  db '      .---------.      ', 0xA
+                  db '    ."           ".    ', 0xA
+                  db '   /               \   ', 0xA
+                  db '  |                 |  ', 0xA
+                  db '   \               /   ', 0xA
+                  db '    ".           ."    ', 0xA
+                  db '      `---------`      ', 0xA
+                  db 0x1B, '[0m'
+    red_frame_len equ $ - red_frame_msg
 
-    yellow_msg db 0x1B, '[38;5;226m'
-               db '      .---------.      ', 0xA
-               db '    ."           ".    ', 0xA
-               db '   /    YELLOW     \   ', 0xA
-               db '  |    GET READY    |  ', 0xA
-               db '   \               /   ', 0xA
-               db '    ".           ."    ', 0xA
-               db '      `---------`      ', 0xA
-               db 0x1B, '[0m', 0xA
-    yellow_len equ $ - yellow_msg
+    yellow_frame_msg db 0x1B, '[1;30m'
+                  db '      .---------.      ', 0xA
+                  db '    ."           ".    ', 0xA
+                  db '   /               \   ', 0xA
+                  db '  |                 |  ', 0xA
+                  db '   \               /   ', 0xA
+                  db '    ".           ."    ', 0xA
+                  db '      `---------`      ', 0xA
+                  db '                       ', 0xA
+                  db 0x1B, '[38;5;226m'
+                  db '      .---------.      ', 0xA
+                  db '    ."           ".    ', 0xA
+                  db '   /    YELLOW     \   ', 0xA
+                  db '  |    GET READY    |  ', 0xA
+                  db '   \               /   ', 0xA
+                  db '    ".           ."    ', 0xA
+                  db '      `---------`      ', 0xA
+                  db 0x1B, '[1;30m'
+                  db '                       ', 0xA
+                  db '      .---------.      ', 0xA
+                  db '    ."           ".    ', 0xA
+                  db '   /               \   ', 0xA
+                  db '  |                 |  ', 0xA
+                  db '   \               /   ', 0xA
+                  db '    ".           ."    ', 0xA
+                  db '      `---------`      ', 0xA
+                  db 0x1B, '[0m'
+    yellow_frame_len equ $ - yellow_frame_msg
 
-    green_msg db 0x1B, '[1;32m'
-              db '      .---------.      ', 0xA
-              db '    ."           ".    ', 0xA
-              db '   /     GREEN     \   ', 0xA
-              db '  |       GO!       |  ', 0xA
-              db '   \               /   ', 0xA
-              db '    ".           ."    ', 0xA
-              db '      `---------`      ', 0xA
-              db 0x1B, '[0m', 0xA
-    green_len equ $ - green_msg
+    green_frame_msg db 0x1B, '[1;30m'
+                  db '      .---------.      ', 0xA
+                  db '    ."           ".    ', 0xA
+                  db '   /               \   ', 0xA
+                  db '  |                 |  ', 0xA
+                  db '   \               /   ', 0xA
+                  db '    ".           ."    ', 0xA
+                  db '      `---------`      ', 0xA
+                  db '                       ', 0xA
+                  db '      .---------.      ', 0xA
+                  db '    ."           ".    ', 0xA
+                  db '   /               \   ', 0xA
+                  db '  |                 |  ', 0xA
+                  db '   \               /   ', 0xA
+                  db '    ".           ."    ', 0xA
+                  db '      `---------`      ', 0xA
+                  db '                       ', 0xA
+                  db 0x1B, '[1;32m'
+                  db '      .---------.      ', 0xA
+                  db '    ."           ".    ', 0xA
+                  db '   /     GREEN     \   ', 0xA
+                  db '  |       GO!       |  ', 0xA
+                  db '   \               /   ', 0xA
+                  db '    ".           ."    ', 0xA
+                  db '      `---------`      ', 0xA
+                  db 0x1B, '[0m'
+    green_frame_len equ $ - green_frame_msg
 
-    ; Blank circle for Night Mode flashing
-    blank_msg db 0x1B, '[1;30m'  ; Dark gray for OFF state
-              db '      .---------.      ', 0xA
-              db '    ."           ".    ', 0xA
-              db '   /               \   ', 0xA
-              db '  |                 |  ', 0xA
-              db '   \               /   ', 0xA
-              db '    ".           ."    ', 0xA
-              db '      `---------`      ', 0xA
-              db 0x1B, '[0m', 0xA
-    blank_len equ $ - blank_msg
+    all_off_frame_msg db 0x1B, '[1;30m'
+                  db '      .---------.      ', 0xA
+                  db '    ."           ".    ', 0xA
+                  db '   /               \   ', 0xA
+                  db '  |                 |  ', 0xA
+                  db '   \               /   ', 0xA
+                  db '    ".           ."    ', 0xA
+                  db '      `---------`      ', 0xA
+                  db '                       ', 0xA
+                  db '      .---------.      ', 0xA
+                  db '    ."           ".    ', 0xA
+                  db '   /               \   ', 0xA
+                  db '  |                 |  ', 0xA
+                  db '   \               /   ', 0xA
+                  db '    ".           ."    ', 0xA
+                  db '      `---------`      ', 0xA
+                  db '                       ', 0xA
+                  db '      .---------.      ', 0xA
+                  db '    ."           ".    ', 0xA
+                  db '   /               \   ', 0xA
+                  db '  |                 |  ', 0xA
+                  db '   \               /   ', 0xA
+                  db '    ".           ."    ', 0xA
+                  db '      `---------`      ', 0xA
+                  db 0x1B, '[0m'
+    all_off_frame_len equ $ - all_off_frame_msg
 
-    ; --- Cursor Control ---
-    ; Moves terminal cursor UP 8 lines and resets to column 1
-    move_up db 0x1B, '[8A', 0x0D
+    ; Cursor Control (Move up exactly 23 lines)
+    move_up db 0x1B, '[23A', 0x0D
     move_up_len equ $ - move_up
 
-    ; --- Sound Control ---
-    ; Raw ASCII Bell byte for accessibility
+    ; Sound Control
     beep_msg db 0x07
     beep_len equ $ - beep_msg
 
-    ; --- Alerts and Prompts ---
+    ; Alerts and Prompts
     ped_alert db 0xA, 0x1B, '[1;36m', '>>> PEDESTRIAN BUTTON PRESSED! <<<', 0x1B, '[0m', 0xA
     ped_alert_len equ $ - ped_alert
 
@@ -68,7 +132,7 @@ section .data
     newline db 0xA
     nl_len equ $ - newline
 
-    ; --- Timer Struct (sys_nanosleep) ---
+    ; Timer Struct
     timeval:
         tv_sec  dq 0
         tv_nsec dq 0
@@ -82,7 +146,7 @@ section .text
 _start:
 
 cycle_start:
-    ; Print a blank line to separate from the prompt
+    ; Print a blank line
     mov rax, 1
     mov rdi, 1
     mov rsi, newline
@@ -92,61 +156,51 @@ cycle_start:
     ; --- STATE 1: RED ---
     mov rax, 1
     mov rdi, 1
-    mov rsi, red_msg
-    mov rdx, red_len
+    mov rsi, red_frame_msg
+    mov rdx, red_frame_len
     syscall
 
-    ; Wait 2.2 seconds
     mov qword [tv_sec], 2
     mov qword [tv_nsec], 200000000
     call do_delay
-
-    ; Move cursor up to overwrite the circle IN-PLACE
     call do_move_up
 
     ; --- STATE 2: YELLOW ---
     mov rax, 1
     mov rdi, 1
-    mov rsi, yellow_msg
-    mov rdx, yellow_len
+    mov rsi, yellow_frame_msg
+    mov rdx, yellow_frame_len
     syscall
 
-    ; Wait 2.2 seconds
     mov qword [tv_sec], 2
     mov qword [tv_nsec], 200000000
     call do_delay
-
-    ; Move cursor up to overwrite IN-PLACE
     call do_move_up
 
     ; --- STATE 3: GREEN ---
     mov rax, 1
     mov rdi, 1
-    mov rsi, green_msg
-    mov rdx, green_len
+    mov rsi, green_frame_msg
+    mov rdx, green_frame_len
     syscall
 
-    ; Wait 3.0 seconds
     mov qword [tv_sec], 3
     mov qword [tv_nsec], 0
     call do_delay
 
 prompt_loop:
-    ; --- INTERACTIVE MENU ---
     mov rax, 1
     mov rdi, 1
     mov rsi, prompt_msg
     mov rdx, prompt_len
     syscall
 
-    ; Read user input
     mov rax, 0
     mov rdi, 0
     mov rsi, input
     mov rdx, 2
     syscall
 
-    ; Branching logic for the Interactive State Machine
     cmp byte [input], 'q'
     je exit
     cmp byte [input], 'n'
@@ -154,61 +208,51 @@ prompt_loop:
     cmp byte [input], 'p'
     je ped_mode
     
-    ; Default: Cycle Again
     jmp cycle_start
 
 ped_mode:
-    ; Alert banner
     mov rax, 1
     mov rdi, 1
     mov rsi, ped_alert
     mov rdx, ped_alert_len
     syscall
 
-    ; GREEN for cars initially
     mov rax, 1
     mov rdi, 1
-    mov rsi, green_msg
-    mov rdx, green_len
+    mov rsi, green_frame_msg
+    mov rdx, green_frame_len
     syscall
     
     mov qword [tv_sec], 2
     mov qword [tv_nsec], 0
     call do_delay
-
     call do_move_up
 
-    ; YELLOW slowing down
     mov rax, 1
     mov rdi, 1
-    mov rsi, yellow_msg
-    mov rdx, yellow_len
+    mov rsi, yellow_frame_msg
+    mov rdx, yellow_frame_len
     syscall
 
     mov qword [tv_sec], 1
     mov qword [tv_nsec], 500000000
     call do_delay
-
     call do_move_up
 
-    ; RED for cars (Pedestrians can cross safely)
     mov rax, 1
     mov rdi, 1
-    mov rsi, red_msg
-    mov rdx, red_len
+    mov rsi, red_frame_msg
+    mov rdx, red_frame_len
     syscall
 
-    ; Beep 4 times over 4 seconds for Visually Impaired Accessibility
     mov r12, 4
 .ped_beep_loop:
-    ; trigger beep
     mov rax, 1
     mov rdi, 1
     mov rsi, beep_msg
     mov rdx, beep_len
     syscall
 
-    ; sleep 1s
     mov qword [tv_sec], 1
     mov qword [tv_nsec], 0
     call do_delay
@@ -219,37 +263,31 @@ ped_mode:
     jmp prompt_loop
 
 night_mode:
-    ; Print blank line first
     mov rax, 1
     mov rdi, 1
     mov rsi, newline
     mov rdx, nl_len
     syscall
 
-    mov r12, 5   ; Flash 5 times
+    mov r12, 5
 .night_loop:
-    ; Print YELLOW
     mov rax, 1
     mov rdi, 1
-    mov rsi, yellow_msg
-    mov rdx, yellow_len
+    mov rsi, yellow_frame_msg
+    mov rdx, yellow_frame_len
     syscall
 
-    ; sleep 0.5s
     mov qword [tv_sec], 0
     mov qword [tv_nsec], 500000000
     call do_delay
-
     call do_move_up
 
-    ; Print BLANK circle
     mov rax, 1
     mov rdi, 1
-    mov rsi, blank_msg
-    mov rdx, blank_len
+    mov rsi, all_off_frame_msg
+    mov rdx, all_off_frame_len
     syscall
 
-    ; sleep 0.5s
     mov qword [tv_sec], 0
     mov qword [tv_nsec], 500000000
     call do_delay
@@ -257,14 +295,12 @@ night_mode:
     dec r12
     jz .night_end
 
-    ; Move up for next flash
     call do_move_up
     jmp .night_loop
 
 .night_end:
     jmp prompt_loop
 
-; --- Subroutine: Move Cursor Up ---
 do_move_up:
     mov rax, 1
     mov rdi, 1
@@ -273,22 +309,19 @@ do_move_up:
     syscall
     ret
 
-; --- Subroutine: Delay ---
 do_delay:
-    mov rax, 35             ; sys_nanosleep (35 in 64-bit Linux kernel)
+    mov rax, 35
     mov rdi, timeval
     xor rsi, rsi
     syscall
     ret
 
-; --- Graceful Exit ---
 exit:
     mov rax, 1
     mov rdi, 1
     mov rsi, newline
     mov rdx, nl_len
     syscall
-
     mov rax, 60
     mov rdi, 0
     syscall
